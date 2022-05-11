75a9c1a0e366833e54dce196cfe0280520775384
Chris Baca <cdbaca@gmail.com>
2022-05-11 15:45:19 -0500

    added make_index function, adds index html w/ post list

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from markdown2 import markdown
 from slugify import slugify
-import jinja2
+from jinja2 import Environment, PackageLoader, FileSystemLoader
 
 # TODO: Get markdown files/data
 def get_files():
@@ -15,13 +15,13 @@ def get_post_content(posts):
         with open(f'posts/{post}','r') as file:
             html_content = markdown(file.read(), extras=['metadata'])
             metadata = html_content.metadata
-            metadata['slug'] = slugify(metadata['title'])
             post_data = {
-                'title': metadata['title'],
-                'slug': slugify(metadata['title']),
-                'author': metadata['author'],
-                'date': metadata['date'],
-                'tags': list(metadata['tags'].split(", ")),
+                'metadata':
+                    {'title': metadata['title'],
+                     'slug': slugify(metadata['title']),
+                     'author': metadata['author'],
+                     'date': metadata['date'],
+                     'tags': list(metadata['tags'].split(", "))},
                 'content': html_content
             }
             post_content.append(post_data)
@@ -29,14 +29,34 @@ def get_post_content(posts):
     return(post_content)
 
 # TODO: Save html to output directory
+def make_posts(post_content):
+
+    pass
+
 # TODO: Generate index
+def make_index(post_content):
+    env = Environment(loader=FileSystemLoader("./templates"))
+    template = env.get_template("index.html")
+
+    post_metadata = []
+    for post in post_content:
+        post_metadata.append(post['metadata'])
+
+    post_metadata.sort(key=lambda x: x['date'], reverse=True)
+
+    rendered = template.render(data=post_metadata)
+
+    os.makedirs('output', exist_ok=True)
+
+    with open('output/index.html', 'w') as f:
+        f.write(rendered)
+
 # TODO: Save style.css to '../output/static'
-# TODO:
 
 def main():
     posts = get_files()
-    contents = get_post_content(posts)
-    print(contents)
+    post_content = get_post_content(posts)
+    make_index(post_content)
 
 
 if __name__ == '__main__':
