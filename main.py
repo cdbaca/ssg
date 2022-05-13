968bcff8bacd796132804fd9e16f5634fc136c47
Chris Baca <cdbaca@gmail.com>
2022-05-13 16:35:54 -0500

    added about function

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ def make_posts(post_content):
     env = Environment(loader=FileSystemLoader("./templates"))
     template = env.get_template("post.html")
 
-    # post_html = []
     for post in post_content:
         rendered = template.render(data=post)
         with open(f'output/{post["metadata"]["slug"]}.html', 'w') as f:
@@ -41,10 +40,7 @@ def make_index(post_content):
     env = Environment(loader=FileSystemLoader("./templates"))
     template = env.get_template("index.html")
 
-    post_metadata = []
-    for post in post_content:
-        post_metadata.append(post['metadata'])
-
+    post_metadata = [post['metadata'] for post in post_content]
     post_metadata.sort(key=lambda x: x['date'], reverse=True)
 
     rendered = template.render(data=post_metadata)
@@ -55,23 +51,31 @@ def make_index(post_content):
         f.write(rendered)
 
 # TODO: make posts page
-def make_post_page(post_content):
-    pass
+def make_about():
+    env = Environment(loader=FileSystemLoader("./templates"))
+    template = env.get_template("about.html")
+    rendered = template.render()
+    with open('output/about.html', 'w') as f:
+        f.write(rendered)
 
 # TODO: Save style.css to '../output/static'
-def make_static():
-    pass
 
 # TODO: deal with tags
+def run_tags(post_content):
+    pass
 
 # TODO: RSS feed
-
+def make_rss():
+    pass
 
 def main():
     posts = get_files()
     post_content = get_post_content(posts)
     make_index(post_content)
     make_posts(post_content)
+    make_about()
+    run_tags(post_content)
+    make_rss()
 
 if __name__ == '__main__':
     main()
