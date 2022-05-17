b22d56506812021aba8e5d1c3aaae2d70ad259ad
Chris Baca <cdbaca@gmail.com>
2022-05-17 12:00:03 -0500

    added support for adding a post via post.py

--- post.py
+++ post.py
@@ -1,1 +1,35 @@
-# TODO: generate template markdown file
+import sys
+import datetime
+from slugify import slugify
+
+def make_frontmatter(title):
+    current_day = datetime.date.today()
+    formatted_date = datetime.date.strftime(current_day, '%m/%d/%Y')
+
+    frontmatter = """---
+title: {0}
+author: Chris
+date: {1}
+tags: 
+---
+
+Write content here.
+""".format(title, formatted_date)
+
+    return frontmatter
+
+def make_template(title, frontmatter):
+    file_name = slugify(title)
+
+    with open(f'posts/{file_name}.md', 'w') as f:
+        f.write(frontmatter)
+
+def main(title):
+    frontmatter = make_frontmatter(title)
+    make_template(title, frontmatter)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        print("Enter title of post.")
---
+++ posts/something-something.md
@@ -0,0 +1,8 @@
+---
+title: something something
+author: Chris
+date: 05/17/2022
+tags: 
+---
+
+Write content here.
